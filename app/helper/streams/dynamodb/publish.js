a2b8b3401d124026e03fa46109a136af7d217069
gshettytcs <g.shetty@tcs.com>
2020-01-02 09:00:02 +0530

    added the sns notification email only.

---
+++ app/helper/streams/dynamodb/publish.js
@@ -0,0 +1,25 @@
+const AWS = require('aws-sdk');
+AWS.config.update({region: 'ap-south-1'});
+
+class SimpleNotificationService {
+    constructor(event){
+        this.sns = new AWS.SNS();
+        this.params = {
+            Message: JSON.stringify({
+                        TableName:  'Gopal',
+                        SensorId : 'sjhad128731283zzcbs,bc01239283'
+                    }),
+            TopicArn: 'arn:aws:sns:ap-south-1:344184308618:aws-serverless-assignment-dev-SensorsEventsDeletionTopic-U06ZAK3NFXU6'
+        };        
+    }
+
+    publish(){
+        try{
+            return this.sns.publish(this.params).then(data => data.MessageId);
+        }catch(error){
+            throw error;
+        }
+    }
+}
+
+module.exports = SimpleNotificationService;
